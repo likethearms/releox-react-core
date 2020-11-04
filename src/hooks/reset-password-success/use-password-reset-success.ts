73b37356c10ad10ce2061e20daf9783a1c4bf5e5
Henrik Salmela <henrik.salmela@gmail.com>
2020-11-04 23:09:42 +0200

    fixing login bug

--- src/hooks/reset-password-success/use-password-reset-success.ts
+++ src/hooks/reset-password-success/use-password-reset-success.ts
@@ -1,11 +1,30 @@
+import { useContext, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import ReleoxConfigContext from '../../config/ReleoxConfigContext';
+import ReleoxLinkObject from '../../ReleoxLinkObject';
 import translationNamespaces from '../../translation-namespaces';
 
 interface ResetPasswordSuccess {
   message: string;
+  links: ReleoxLinkObject[];
 }
 
 export default (): ResetPasswordSuccess => {
+  const [links, setLinks] = useState<ReleoxLinkObject[]>([]);
+
+  const [config] = useContext(ReleoxConfigContext);
+
   const { t } = useTranslation(translationNamespaces.resetPasswordSuccess);
-  return { message: t('message') };
+
+  useEffect(() => {
+    setLinks([
+      {
+        id: 'back-link',
+        to: config.routes.login,
+        text: t('toLogin'),
+      },
+    ]);
+  }, [setLinks, config.routes.login, t]);
+
+  return { message: t('message'), links };
 };
